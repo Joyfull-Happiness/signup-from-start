cc58fffd54f33b372eff66b5ef0da596204e9e76
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-04-24 13:55:34 -0700

    updated js

--- api.js
+++ api.js
@@ -1,31 +1,30 @@
-let container = document.quereySelector(".container");
+let container = document.querySelector(".container");
 
-let form = document.quereySelector("form");
-form.addEventlistener("submit", onformsubmit);
-console.log("form:", form); 
+let form = document.querySelector("form");
+form.addEventListener("submit", onformsubmit);
+console.log("form:", form);
 
-function onformsubmit(){
-    event.preventDeafult();
-    const data = new FormData(event.target);
-    const dataObject = Object.fromEntries(data.entries());
-    console.log(dataObject);
+function onformsubmit() {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+  console.log("dataObject:", dataObject);
 
-    form.rest();
+  form.reset();
 
-    let firstName = dataObject.firstName;
-    let lastName  = dataObject.lastName;
-    let email = dataObject.email;
-    let password = dataObject.password;
+  let firstName = dataObject.firstName;
+  let lastName = dataObject.lastName;
+  let email = dataObject.email;
+  let password = dataObject.password;
 
-    let message = `congrats ${firstName} on your free trial!`
+  let message = `congrats ${firstName} on your free trial!`;
 
-    console.log("first message to user:", message); 
+  console.log("first message to user:", message);
 
-    let messageParagraph = document.createElement("p");
-    messageParagraph.textContent = message;
-    container.appendChild(messageParagraph);
+  let messageParagraph = document.createElement("p");
+  messageParagraph.textContent = message;
+  container.appendChild(messageParagraph);
 }
-)
 
 /* 
 Code the following design for desktop only:
